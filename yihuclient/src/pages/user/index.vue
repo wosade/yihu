2231d31377425d2473b007bb742de8d6bf6d2c88
jiang <jiang@qq.com>
2026-04-16 20:23:17 +0800

    fix: 推送用户页面更新

--- yihuclient/src/pages/user/index.vue
+++ yihuclient/src/pages/user/index.vue
@@ -24,10 +24,8 @@ const quit=()=>{
 <template>
   <div class="container">
     <div class="user">
-      <van-image :src="userInfo.avatar" width="100px" class="img" >
-        <template #error>
-          <van-image :src="fallbackAvatar" width="100px" class="img" />
-        </template>
+      <van-image :src="userInfo.avatar|| fallbackAvatar" width="100px" class="img" >
+        
       </van-image>
       <div class="text">{{ userInfo.name }}</div>
     </div>
